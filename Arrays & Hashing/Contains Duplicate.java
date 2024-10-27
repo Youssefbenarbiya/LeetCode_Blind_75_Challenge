1635a6f491aae4a33b39b04d6b1703c53cb5ff36
YOUSEF BENARBIA <benarbiayousef@nabeul.r-iset.tn>
2024-10-27 10:42:38 +0100

    Update Contains Duplicate.java

--- Arrays & Hashing/Contains Duplicate.java
+++ Arrays & Hashing/Contains Duplicate.java
@@ -1,11 +1,11 @@
-class Solution {
+public class Solution {
     public boolean hasDuplicate(int[] nums) {
-        for(int i = 0 ; i < nums.length; i++){
-            for( int j = i + 1 ; j < nums.length; j++){
-                if(nums[i] == nums[j]){
-                    return true;
-                }
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
             }
+            seen.add(num);
         }
         return false;
     }
